98c471ea0a1c776a36e986a5a541ea9841adac14
barteqPC <barteq2011@gmail.com>
2021-02-09 16:11:21 +0100

    Initial Commit

---
+++ src/test/java/com/kirg/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirg.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
